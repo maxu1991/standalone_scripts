d98344e868da1e128d65e3f81e5f525907518629
maxu1991 <m.yurcev@yandex.ru>
2018-07-03 17:06:35 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,14 +1,14 @@
 
 If (![bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{Throw "Требуется запуск от имени администратора!"}
+{Throw "Launch with administrator rights"}
 Invoke-WebRequest -Uri 'http://download.eset.com/download/sysinspector/64/ENU/SysInspector.exe' -outfile "$env:public\SysInspector.exe"
 cd $env:PUBLIC
 $Filename = "$env:PUBLIC\SysInspector-$env:Computername-$(Get-date -format 'yyMMdd-hhmmss').zip"
-Write-Host "SysInspector запущен, дождитесь окончания сбора журналов"
+Write-Host "SysInspector working, wait"
 $ProcessHandle = Start-Process "$env:PUBLIC\sysinspector.exe" -ArgumentList "/gen=$Filename /silent /privacy /zip" -Passthru
 Do {
     ++$ElapsedTime
     Start-Sleep -Seconds 60
-    Write-Host "Прошло $ElapsedTime минут"
+    Write-Host "Work $ElapsedTime min"
 } until ($ProcessHandle.HasExited -eq $True)
-Write-Host "Пожалуйста отправьте нам -  `"$Filename`""
+Write-Host "Please send a create file -  `"$Filename`""
